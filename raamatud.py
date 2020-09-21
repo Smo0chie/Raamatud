51f4f2b792f2a1296e36123f7c5259c74ff562d9
ReenaSeeba <70514155+ReenaSeeba@users.noreply.github.com>
2020-09-21 11:11:58 +0300

    Add files via upload

---
+++ raamatud.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+raamatute_seeria = pd.Series({'G. Orwell "1984"': 'loetud',
+                              'V. Aveyard "Red Queen"': 'pooleli',
+                              'J Niven "All the Brigth Places"': 'lugemata'})
+print(raamatute_seeria)
