ec89f6f0e86c84ea938370117fe8cb3d08885ea4
ReenaSeeba <70514155+ReenaSeeba@users.noreply.github.com>
2020-10-05 23:17:07 +0300

    Add files via upload

--- raamat_2.0.py
+++ raamat_2.0.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#andmebaasi kood, mis jääb muutumatujs sõltumata kasutajaliidesest
 def db_loe_raamatud():
     return pd.read_csv('raamatukogu.csv', encoding='UTF-8')
 
@@ -13,24 +14,45 @@ def db_lisa_raamat(autor, pealkiri, staatus):
     raamatud.to_csv('raamatukogu.csv', encoding='utf-8', index = False)
     return raamatud
 
-def loe_ribakood(failinimi):
+def raamat_ribakoodist(failinimi):
     import cv2
+    import isbnlib
     from pyzbar.pyzbar import decode
-    return decode(cv2.imread(failinimi))[0].data
+    isbn = decode(cv2.imread(failinimi))[0].data.decode('ascii')
+    meta = isbnlib.meta(isbn)
+    return {
+        'Autor':    meta['Authors'][0],
+        'Pealkiri': meta['Title']
+    }
     
-
-def kr_lisamine():
-    uue_raamatu_autor    = input("Autori nimi: ")
-    uue_raamatu_pealkiri = input("Raamatu pealkiri: ")
+#kasutajaliidesega asendada
+def kr_lisamine(ribakoodi_failinimi = None):
+    if ribakoodi_failinimi:
+        raamat = raamat_ribakoodist(ribakoodi_failinimi)
+        uue_raamatu_autor    = raamat['Autor']
+        uue_raamatu_pealkiri = raamat['Pealkiri']
+        print('Autor: ',    raamat['Autor'])
+        print('Pealkiri: ', raamat['Pealkiri'])
+    else:
+        uue_raamatu_autor    = input("Autori nimi: ")
+        uue_raamatu_pealkiri = input("Raamatu pealkiri: ")
     uue_raamatu_staatus  = input("loetud/poolei/lugemata: ")
-    
     koik_raamatud = db_lisa_raamat(uue_raamatu_autor, uue_raamatu_pealkiri, uue_raamatu_staatus)
-    
     print(koik_raamatud)
 
-kr_lisamine()
-#print(db_loe_raamatud())
-#print(loe_ribakood('ribakood.png'))
+while True:
+    tegevus = input('Skanneeri(1), lisa raamat käsitsi(2), näita andmebaasi(3), lõpeta(4): ')
+    if tegevus == '1':
+        kr_lisamine(input('Sisesta pildi faili nimi: '))
+    elif tegevus == '2':
+        kr_lisamine()
+    elif tegevus == '3':
+        print(db_loe_raamatud())
+    elif tegevus == '4':
+        break
+    else:
+        print('Tundmatu valik.')
+        
 
 
 
